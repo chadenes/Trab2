ac5585aa8c3c9098ef765b523a3d23dde1f7ff46
chadenes <rafael.ignan.machado@usp.br>
2015-11-04 18:02:51 -0200

    evoluindo

--- Operações básicas em árvores.c
+++ Operações básicas em árvores.c
@@ -28,16 +28,39 @@ void inserir (no **raiz, int k){//função que insere novos nós na árvore
 
 
 
+no  busca (no **raiz, int k) {//função que busca determinado elemento e devolve o nó em que ele se encontra, devolve null caso ele n exista
+    if (*raiz == NULL){
+    	printf("O elemento nao se encontra na arvore");
+    	return;
+	}
+	if( (*raiz)->chave == k ){
+		printf("O elemento se encontra na arvore");
+		return;
+	}
+    if ((*raiz)->chave > k)
+       return busca (&(*raiz)->esq, k);
+    else
+       return busca (&(*raiz)->dir, k);
+}
+
+
+
+
+
+
 void main(){
 	no *raiz = NULL;
 	int n, i, elemento;
 	printf("Ola, por favor insira quantidade de numeros com que voce ira trabalhar \n nesta arvore:");
-	scanf("%d", &n);
+	scanf("%d", &n);  //recebo o número de elementos que o usuário deseja utilizar
 	printf("Agora insira os elementos:\n");
-	for(i=1;i<=n;i++){
+	for(i=1;i<=n;i++){             //usando um laço de repetição eu recolho o valor de todos os elementos da árvore
 		printf("Elemento %d: ", i);
 		scanf("%d", &elemento);
 		inserir(&raiz, elemento);
 	}
+	printf("Escolha um elemento para verificar se ele esta na arvore: ");
+	scanf("%d", &elemento);
+	busca(&raiz, elemento);
 	return ;
 }
