15ed986cb4579895e03c1c0a05fffdc7a4faab2d
chadenes <rafael.ignan.machado@usp.br>
2015-11-04 21:54:33 -0200

    Versão1.9

--- Operações básicas em árvores.c
+++ Operações básicas em árvores.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/* No início do programa estão escritas todas as funções que serão utilizadas 
+ao decorrer do mesmo */
+
+
+
+
 struct arvore{           //criado o tipo de struct que irá compor nossa árvore de busca
 	int chave;
 	struct arvore *esq;
@@ -19,7 +25,8 @@ void inserir (no **raiz, int k){//função que insere novos nós na árvore
 	}
 	if((*raiz)->chave > k){        //usando recursão fazemos com que o nó, se maior que o elemento analisado á para a direita 
 		inserir(&(*raiz)->esq, k);//e se for menor para a esquerda
-	}else{
+	}
+	if((*raiz)->chave<k){
 		inserir(&(*raiz)->dir, k);
 	}
 }
@@ -30,21 +37,52 @@ void inserir (no **raiz, int k){//função que insere novos nós na árvore
 
 no  busca (no **raiz, int k) {//função que busca determinado elemento e devolve o nó em que ele se encontra, devolve null caso ele n exista
     if (*raiz == NULL){
-    	printf("O elemento nao se encontra na arvore");
-    	return;
+    	printf("O elemento %d nao se encontra na arvore", k); //se o elemento não se encontrar na árvore é devolvida uma 
+    	return;                                         //mensagem avisando isso
 	}
 	if( (*raiz)->chave == k ){
-		printf("O elemento se encontra na arvore");
+		printf("O elemento %d se encontra na arvore", k); //se o elemento se encontrar na árvore é devolvida uma mensagem avisando isso
 		return;
 	}
-    if ((*raiz)->chave > k)
-       return busca (&(*raiz)->esq, k);
+    if ((*raiz)->chave > k)     //essa parte da função indica "aonde" procurar o número, sabendo que os números que são
+       return busca (&(*raiz)->esq, k); //maiores "vão" para a direita e os menores pra esquerda
     else
        return busca (&(*raiz)->dir, k);
 }
 
+no *doisfilhos(no *raiz){ //essa função foi criada para auxiliar na remoção de um nó que tem dois filhos
+	if(raiz == NULL){     //foi necessária a criação da função pois os próprios filhos daquele nó podem ter 2 filhos
+		return NULL;
+	}else if(raiz->esq = NULL){
+		return raiz;
+	}else{
+		return doisfilhos(raiz->esq);
+	}
+}
 
 
+void remover (no **raiz, int elemento){     //função eu remove o valor desejado
+	if(elemento < (*raiz)->chave){          //no caso de estarmos removendo uma função que tem um filho ou nenhum o problema se torna fácil
+		remover(&(*raiz)->esq, elemento);
+	}else if(elemento > (*raiz)->chave){
+		remover(&(*raiz)->dir, elemento);
+	}else if((*raiz)->esq!=NULL && (*raiz)->dir!=NULL){
+		no *aux = NULL;
+		aux = doisfilhos((*raiz)->dir);   //porém para remover um nó com dois filhos o problema se torna mais complicado
+		(*raiz)->chave = aux->chave;      //para isso foi criada a função doisfilhos
+		remover(&(*raiz)->dir, (*raiz)->chave);
+	}
+	else{
+		no *aux = (*raiz);
+		if((*raiz)->esq==NULL){
+			(*raiz)=(*raiz)->dir;
+		}
+		else{
+			(*raiz)=(*raiz)->esq;
+		}
+		free(aux);
+	}
+}
 
 
 
@@ -61,6 +99,10 @@ void main(){
 	}
 	printf("Escolha um elemento para verificar se ele esta na arvore: ");
 	scanf("%d", &elemento);
+	busca(&raiz, elemento);
+	printf("\nAgora escolha um elemento para remover da arvore: ");
+	scanf("%d", &elemento);
+	remover(&raiz, elemento);
 	busca(&raiz, elemento);
 	return ;
 }
