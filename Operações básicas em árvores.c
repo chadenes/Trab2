06e23aff62d319c5a3d55c38082accbd7348a2cb
chadenes <rafael.ignan.machado@usp.br>
2015-11-04 15:57:47 -0200

    Save inicial

---
+++ Operações básicas em árvores.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct arvore{
+	int chave;
+	struct arvore *esq;
+	struct arvore *dir;
+};
+typedef struct arvore no; //definindo essa struct como nó, para ficar mais rápido de escrever
+
+
+void inserir (no *raiz, int k){//função inserção
+	
+}
